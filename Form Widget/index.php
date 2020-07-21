01db9335e743901433cc5123b81f5cc2b93a0a43
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-21 09:53:36 +0400

    Create Initial version of Form Widget project

--- Form Widget/index.php
+++ Form Widget/index.php
@@ -1,6 +1,17 @@
 <?php
 
+require_once __DIR__ . '/Form.php';
+require_once __DIR__ . '/Button.php';
+require_once __DIR__ . '/TextInput.php';
+require_once __DIR__ . '/PasswordInput.php';
+
+$form = new Form();
+$form->addElement(new TextInput('firstname', 'First name'));
+$form->addElement(new TextInput('email', 'Email'));
+$form->addElement(new PasswordInput('password', 'Password'));
+$form->addElement(new Button("Submit"));
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,6 +21,6 @@
     <title>Form Widget</title>
 </head>
 <body>
-<!--Render Form here-->
+<?php echo $form->render() ?>
 </body>
 </html>
