ce9ce8d5941b4273a5f4c73e35bdd0922671b7e6
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-07-21 09:43:23 +0400

    Add empty project for Form Widget

---
+++ Form Widget/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Form Widget</title>
+</head>
+<body>
+<!--Render Form here-->
+</body>
+</html>
